55e412e653112467e5906aa2799c566c6392c091
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-03 17:49:18 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -16,6 +16,40 @@ RADIO_STATIONS = {
     "river_nile": "http://104.7.66.64:8087",
     "hajj_channel": "http://104.7.66.64:8005",
     "omar_kafi": "http://104.7.66.64:8007",
+    "rubat_ataq": "http://stream.zeno.fm/5tpfc8d7xqruv",
+    "shahul_radio": "https://stream-150.zeno.fm/cynbm5ngx38uv?zs=Ktca5StNRWm-sdIR7GloVg",
+    "eram_fm": "http://icecast2.edisimo.com:8000/eramfm.mp3",
+    "abc_islam": "http://s10.voscast.com:9276/stream",
+    "al_sumood_fm": "http://us3.internet-radio.com/proxy/alsumoodfm?mp=/stream",
+    "nur_ala_nur": "http://104.7.66.64:8011/",
+    "seiyun_radio": "http://s2.radio.co/s26c62011e/listen",
+    "real_fm": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio083/playlist.m3u8",
+    "noor_al_eman": "http://edge.mixlr.com/channel/boaht",
+    "radio_keralam": "http://ice31.securenetsystems.net/RADIOKERAL",
+    "al_nour": "http://audiostreaming.itworkscdn.com:9066/",
+    "muthnabi_radio": "http://cast4.my-control-panel.com/proxy/muthnabi/stream",
+    "fm_gold": "https://airhlspush.pc.cdn.bitgravity.com/httppush/hispbaudio005/hispbaudio00564kbps.m3u8",
+    "malayalam_90s": "https://stream-159.zeno.fm/gm3g9amzm0hvv?zs-x-7jq8ks_TOav9ZhlYHi9xw",
+    "aural_oldies": "https://stream-162.zeno.fm/tksfwb1mgzzuv?zs=SxeQj1-7R0alsZSWJie5eQ",
+    "radio_malayalam": "https://radiomalayalamfm.com/radio/8000/radio.mp3",
+    "swaranjali": "https://stream-161.zeno.fm/x7mve2vt01zuv?zs-D4nK05-7SSK2FZAsvumh2w",
+    "air_kozhikode": "https://air.pc.cdn.bitgravity.com/air/live/pbaudio082/chunklist.m3u8",
+    "sam_yemen": "https://edge.mixlr.com/channel/kijwr",
+    "afaq": "https://edge.mixlr.com/channel/rumps",
+    "al_jazeera": "http://live-his-audio-web-aja.getaj.net/VOICE-AJA/index.m3u8",
+    "alfasi_radio": "https://qurango.net/radio/mishary_alafasi",
+    "tafsir_quran": "https://radio.quranradiotafsir.com/9992/stream",
+    "malayalam_1": "http://167.114.131.90:5412/stream",
+    "urdu_islamic_lecture": "http://144.91.121.54:27001/channel_02.aac",
+    "hob_al_nabi": "http://216.245.210.78:8098/stream",
+    "sanaa_radio": "http://dc5.serverse.com/proxy/pbmhbvxs/stream",
+    "radio_beat_malayalam": "http://live.exertion.in:8050/radio.mp3",
+    "nonstop_hindi": "http://s5.voscast.com:8216/stream",
+    "radio_digital_malayali": "https://radio.digitalmalayali.in/listen/stream/radio.mp3",
+    "quran_radio_cairo": "http://n02.radiojar.com/8s5u5tpdtwzuv?listening-from-radio-garden-1620219571863&rj-ttl=5&rj-tok=AAABivzcwaEAh735PZBqcATySw",
+    "quran_radio_nablus": "http://www.quran-radio.org:8002/",
+    "allahu_akbar_radio": "http://66.45.232.132.9996/",
+    "yemen_talk_2": "http://stream.zeno.fm/7qv7c8eq7hhvv",
     "24_news": "https://www.youtube.com/@24OnLive/live",
     "safari_tv": "https://j78dp346yq5r-hls-live.5centscdn.com/safari/live.stream/chunks.m3u8",
     "victers_tv": "https://932y4x26ljv8-hls-live.5centscdn.com/victers/tv.stream/victers/tv1/chunks.m3u8",
@@ -26,7 +60,7 @@ RADIO_STATIONS = {
     "valiyudheen_faizy": "https://www.youtube.com/@voiceofvaliyudheenfaizy600/live",
     "skicr_tv": "https://www.youtube.com/@SKICRTV/live",
     "yaqeen_institute": "https://www.youtube.com/@yaqeeninstituteofficial/live",
-    "bayyinah_tv": "https://www.youtube.com/@bayyinah/live",      
+    "bayyinah_tv": "https://www.youtube.com/@bayyinah/live",
 }
 
 def get_youtube_audio_url(youtube_url):
@@ -49,39 +83,26 @@ def get_youtube_audio_url(youtube_url):
     return None
 
 def generate_stream(url):
-    """Transcodes and serves audio using FFmpeg with reconnect options for stability."""
+    """Transcodes and serves audio using FFmpeg."""
     process = subprocess.Popen(
-        [
-            "ffmpeg",
-            "-reconnect", "1",
-            "-reconnect_streamed", "1",
-            "-reconnect_delay_max", "5",
-            "-i", url,  
-            "-vn",              # Disable video
-            "-b:a", "64k",      # Set audio bitrate
-            "-f", "mp3",        # Output format
-            "-"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
+        ["ffmpeg", "-reconnect", "1", "-reconnect_streamed", "1", "-reconnect_delay_max", "5",
+         "-i", url, "-vn", "-b:a", "64k", "-f", "mp3", "-"],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process.stdout
 
 @app.route("/<station_name>")
 def stream(station_name):
     """Serve the requested station as a live stream."""
-    if station_name in RADIO_STATIONS:
-        url = RADIO_STATIONS[station_name]
+    url = RADIO_STATIONS.get(station_name)
+    if not url:
+        return "Station not found", 404
 
-        # If it's a YouTube Live link, extract the direct audio URL
-        if "youtube.com" in url or "youtu.be" in url:
-            url = get_youtube_audio_url(url)
-            if not url:
-                return "Failed to get YouTube stream", 500
+    if "youtube.com" in url or "youtu.be" in url:
+        url = get_youtube_audio_url(url)
+        if not url:
+            return "Failed to get YouTube stream", 500
 
-        return Response(generate_stream(url), mimetype="audio/mpeg")
-
-    return "Station not found", 404
+    return Response(generate_stream(url), mimetype="audio/mpeg")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
