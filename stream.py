209413d9217ab5b216f96cb77007504f2b456e67
nallion <97355641+nallion@users.noreply.github.com>
2025-03-10 20:41:28 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -21,13 +21,13 @@ def generate_stream(url):
             [
                 "ffmpeg", "-reconnect", "1", "-reconnect_streamed", "1", "-reconnect_delay_max", "10", "-i", url, "-vn", "-ac", "1", "-acodec", "libmp3lame", "-b:a", "40k", "-ar", "32000", "-buffer_size", "2048k", "-f", "mp3", "-"
             ],
-            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=8192
+            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=-1
         )
 
         print(f"🎵 Streaming from: {url} (Mono, 40kbps)")
 
         try:
-            for chunk in iter(lambda: process.stdout.read(8192), b""):
+            for chunk in iter(lambda: process.stdout.read(1024), b""):
                 yield chunk
         except GeneratorExit:
             process.kill()
