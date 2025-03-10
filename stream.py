759d72014fa2ebc85d699aac704a9315b64f18a4
nallion <97355641+nallion@users.noreply.github.com>
2025-03-10 20:25:29 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -50,7 +50,15 @@ def stream(station_name):
     if not url:
         return "⚠️ Station not found", 404
     
-    return Response(generate_stream(url), mimetype="audio/mpeg")
+      return Response(
+        response(),
+        headers={
+            # NOTE: Ensure stream is not cached.
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            'Pragma': 'no-cache',
+            'Expires': '0',
+        },
+        mimetype='audio/mpeg')
 
 # 🚀 Start Flask server
 if __name__ == "__main__":
