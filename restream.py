456d6f705dbce697892ab5bc4cbc2da79232ed27
nallion <97355641+nallion@users.noreply.github.com>
2025-03-11 20:53:07 +0100

    Update restream.py

--- restream.py
+++ restream.py
@@ -32,7 +32,7 @@ class FFmpegHandler(BaseHTTPRequestHandler):
 
         # Start FFmpeg process to read from the selected stream
         process = subprocess.Popen(
-            ['ffmpeg', '-i', stream_url, '-ab', '32k', '-ar', '32000', '-ac', '1', '-f', 'mp3', '-'],
+            ['ffmpeg', '-i', stream_url, '-ab', '40k', '-ar', '32000', '-ac', '1', '-f', 'mp3', '-'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
