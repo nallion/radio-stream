ec8747689fa15bac26c54d788b96af668c707160
nallion <97355641+nallion@users.noreply.github.com>
2025-03-15 04:03:27 +0100

    added: Дорожное радио

--- restream.py
+++ restream.py
@@ -6,6 +6,7 @@ import threading
 
 # Define your available radio streams
 RADIO_STREAMS = {
+    'dor': 'https://hls-01-dorognoe.emgsound.ru/15/112/playlist.m3u8',
     'rock': 'https://stream.pcradio.ru/Rock-hi',
     'rrd': 'https://stream1.radiord.ru:8000/live128.mp3',
     'angels': 'http://myradio24.org/angels',
