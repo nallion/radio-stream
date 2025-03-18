55a17b80a63f35e5d0ec5f39fa072f84d69a9b0e
nallion <97355641+nallion@users.noreply.github.com>
2025-03-18 08:24:02 +0100

    Update restream.py

--- restream.py
+++ restream.py
@@ -47,7 +47,7 @@ class FFmpegHandler(BaseHTTPRequestHandler):
 
         # Start FFmpeg process to read from the selected stream
         process = subprocess.Popen(
-            ['/usr/bin/ffmpeg', '-i', stream_url, '-acodec', 'libfdk_aac', '-profile:a', 'aac_he_v2', '-ab', '20k', '-f', 'adts', '-'],
+            ['/usr/local/bin/ffmpeg', '-i', stream_url, '-acodec', 'libfdk_aac', '-profile:a', 'aac_he_v2', '-ab', '20k', '-f', 'adts', '-'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
