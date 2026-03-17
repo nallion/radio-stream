df2d17fd0a2bda7896394a19d5ddb3f2fea8dcbc
nallion <97355641+nallion@users.noreply.github.com>
2026-03-17 12:33:31 +0100

    Update mtproxy.php

--- mtproxy.php
+++ mtproxy.php
@@ -1,7 +1,2 @@
 <?php
-echo "tg.vpnspacev.com:443:bc184fc14b62b9b1dc5f34edf9476421\r\n";
 echo "109.120.190.5:443:9f3c7a8d2e4b1c6f5a7d8e9b0c2f4a1d\r\n";
-echo "179.43.191.102:443:9f3c7a8d2e4b1c6f5a7d8e9b0c2f4a1d\r\n";
-echo "tg.iloveyou-russia.ru:443:b3a2c62bcd96ee97e40ce5a89ca58f8d\r\n";
-echo "khoda.hafez.info:443:ee1603010200010001fc030386e24c3add626973636f7474692e79656b74616e65742e636f6d\r\n";
-echo "185.117.0.248:443:eed451f808fd60ed2c45f11d38fdbc87c57961686f6f2e636f6d\r\n";
