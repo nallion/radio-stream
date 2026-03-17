38af74d6e7d2a3b49cb06884a90d7816b8869c1b
nallion <97355641+nallion@users.noreply.github.com>
2026-03-17 11:40:51 +0100

    Update mtproxy.php

--- mtproxy.php
+++ mtproxy.php
@@ -1,12 +1,7 @@
-<php
-echo "tg.vpnspacev.com:443:bc184fc14b62b9b1dc5f34edf9476421";
-echo '\n';
-echo "109.120.190.5:443:9f3c7a8d2e4b1c6f5a7d8e9b0c2f4a1d";
-echo '\n';
-echo "179.43.191.102:443:9f3c7a8d2e4b1c6f5a7d8e9b0c2f4a1d";
-echo '\n';
-echo "tg.iloveyou-russia.ru:443:b3a2c62bcd96ee97e40ce5a89ca58f8d";
-echo '\n';
-echo "khoda.hafez.info:443:ee1603010200010001fc030386e24c3add626973636f7474692e79656b74616e65742e636f6d";
-echo '\n';
-echo "185.117.0.248:443:eed451f808fd60ed2c45f11d38fdbc87c57961686f6f2e636f6d";
+<?php
+echo "tg.vpnspacev.com:443:bc184fc14b62b9b1dc5f34edf9476421\n";
+echo "109.120.190.5:443:9f3c7a8d2e4b1c6f5a7d8e9b0c2f4a1d\n";
+echo "179.43.191.102:443:9f3c7a8d2e4b1c6f5a7d8e9b0c2f4a1d\n";
+echo "tg.iloveyou-russia.ru:443:b3a2c62bcd96ee97e40ce5a89ca58f8d\n";
+echo "khoda.hafez.info:443:ee1603010200010001fc030386e24c3add626973636f7474692e79656b74616e65742e636f6d\n";
+echo "185.117.0.248:443:eed451f808fd60ed2c45f11d38fdbc87c57961686f6f2e636f6d\n";
